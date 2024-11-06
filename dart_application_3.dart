1f6cd47c1b5381a8acc07455d28852fd3a28dc14
Rajikuzzaman2001 <mdarpon2045@gmail.com>
2024-11-06 22:47:15 +0600

    Add files via upload

---
+++ dart_application_3.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/matarial.dart';
+
+Run||Debug|profile
+void main ()
+runApp (MyApp());
+printf("hi everyone");
+printf(9=9);
+{
+
+    class MyApp extends Statelesswidget {
+        const Myapp{super,key}
+        //VARRIABILES:You can store different type of info into VARRIABILE
+        String name:="Arpon";
+        int age=24;
+        bool is beginner=true;
+
+        //Basic Math OPerations;
+
+        /*
+        1+1=>2,add
+        4-1=>3,subtract
+        3*2=>6,multipy
+        8/4=2,divide
+        9%4=>1,remainder
+
+        COMPARISON OPERATIONS;
+        5==5->true, EQUAL TO
+        2!=3->true, NOT EQUAL TO
+        3>2-> true,GREATER THAN
+        3<2-> false,LESS THAN
+        5>==5->true,GREATER OR EQUAL TO
+        3=7->true,LESS THEN OR EQUAL TO
+
+        CONDITION FLOW
+         if (condition) {do something}
+         if () {
+            else }
+            {
+
+            }
+            if() {
+
+            } else if {
+
+            }
+
+        
+        */
+
+    };
+
+    //basic function
+    void great (){
+        print ("Hellow,Arpon"); 
+    }
+
+    //function with parameters
+    void greatperson(string name,int age) {
+        print("Hellow",+name);
+    }
+
+//function with return type
+int add (int a, int b){
+  int sum = a+b;
+  return um;
+}
+
+D A T A S T R U C T U R E S
+//LIST;order collection of elements,can have duplications,
+List<String> names = ("Arpon","Alvi","Asif");
+//names [0]->Arpon
+//names [1]->Alvi
+//names [2]->Asif
+
+// SET: unordered collection of unique elements
+Set<String> uniqueNames = {"Arpon","Alvi","Asif"};
+
+map user = {
+    'name':Arpon
+    'age':24
+    'height': 180,
+}
+
+List number = ( 1,2,3,);
+for (int i=0; i< numbers.length;i++){
+  print(number[i]);
+}
+    @override
+    widget build (Buildcontext contest){
+        if (your age =24){
+            printf("you are young");
+        }else (your age is =>15){
+            printf("you are not young);
+        }
+printt(user['age']);
+        {
+            string grade=("A");
+            if (grade="A");{
+                printf("excellent");
+            }else if(grade="B");{
+            printf("good");}
+            else if(grade="c");{
+                printf("fair");
+            }
+
+            }
+        }
+        //for loop
+
+        for (initialaization;condition;iretation;);{
+            for(i=0;i>5;i++)
+            print("i");
+        }
+
+        while loop {condition}
+
+        break  ->break out  of the rule
+        continue  ->skip the current iretation
+
+        int coundown=5;
+        while (coundown>0);{
+            print(coundown)
+        
+        --,coundown;
+        }
+        int mysum = add (3,5);
+        print(mysum);
+         
+        
+
+        return materialApp(debugshowCheakedmodeBanner:false,
+        home:scaffold(),)
+    }
+}
